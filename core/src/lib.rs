59b29bff794f167f1c9111bdabb56e0d6923a6b2
Justin Maillet <justy777@icloud.com>
2025-08-15 13:38:03 -0300

    Define lint levels in workspace

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,13 +3,6 @@
 //! This library holds the emulated hardware for a Game Boy (DMG) that can be reused
 //! and re-exported in different implementations.
 
-#![allow(
-    clippy::cast_lossless,
-    clippy::module_name_repetitions,
-    clippy::option_if_let_else,
-    clippy::verbose_bit_mask
-)]
-
 pub mod cartridge;
 pub mod hardware;
 
